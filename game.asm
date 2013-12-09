cdfd7bae8730f24b4ecbed3b214c8fe0e11eba14
Daiderd Jordan <daiderd@gmail.com>
2013-12-09 14:39:32 +0100

    use Game_data
    - use lea instead of mov offset
    - Level_one

--- game.asm
+++ game.asm
@@ -4,19 +4,49 @@ include game.inc
 include screen.inc
 include input.inc
 
-TAG_kColor     equ 00h
-TAG_kDirection equ 02h
-TAG_kPosition  equ 04h
-TAG_kIndex     equ 06h
-TAG_kTail      equ 08h
+TAG_kColor      equ 00h
+TAG_kDirection  equ 02h
+TAG_kPosition   equ 04h
+TAG_kIndex      equ 06h
+TAG_kPlayerSize equ 08h
+
+TAG_kPlayerLeft  equ 0*TAG_kPlayerSize
+TAG_kPlayerRight equ 1*TAG_kPlayerSize
+TAG_kLevel       equ 2*TAG_kPlayerSize
+TAG_kTails       equ TAG_kLevel + 4*Screen_kWidth + 4*Screen_kHeight
 
 .FARDATA
 
-Player_left       dw 16000 dup(0)
-Player_right      dw 16000 dup(0)
+Game_data         dw 32000 dup(0)
 Player_directions dw 5 dup(0) ; Jump table with label offsets
 
 .CODE
+
+Player_left macro dest ; dest <- Offset player {{{1
+  lea dest, [Game_data + TAG_kPlayerLeft]
+endm
+
+Player_leftGet macro tag ; ax <- Player[tag] {{{1
+  mov ax, [Game_data + TAG_kPlayerLeft][tag]
+endm
+
+Player_leftSet macro tag, src ; Player[tag] <- src {{{1
+  mov [Game_data + TAG_kPlayerLeft][tag], src
+endm
+
+Player_right macro dest ; dest <- Offset player {{{1
+  lea dest, [Game_data + TAG_kPlayerRight]
+endm
+
+Player_rightGet macro tag ; ax <- Player[tag] {{{1
+  mov ax, [Game_data + TAG_kPlayerRight][tag]
+endm
+
+Player_rightSet macro tag, src ; Player[tag] <- src {{{1
+  mov [Game_data + TAG_kPlayerRight][tag], src
+endm
+
+; }}}1
 
 assume ds:@fardata, es:@fardata
 
@@ -33,19 +63,21 @@ Game_run proc far ; IO () {{{1
 @@:
   call Player_input
 
-  mov  ax, offset Player_left
+  Player_left ax
   push ax
   call Player_update ; (player)
-  mov  ax, offset Player_right
+  Player_right ax
   push ax
   call Player_update ; (player)
 
-  mov  ax, offset Player_left
+  Player_left ax
   push ax
   call Player_render ; (player)
-  mov  ax, offset Player_right
+  Player_right ax
   push ax
   call Player_render ; (player)
+
+  call Level_render
 
   call Screen_update
 
@@ -66,6 +98,7 @@ Game_setup proc far ; IO () {{{1
 
   call Player_alloc
   call Player_init
+  call Level_init
 
   mov sp, bp
   pop bp
@@ -84,33 +117,32 @@ Player_alloc proc near ; IO () {{{1
   ; Allocate left player
   mov al, Screen_kCyan ; Color (tail)
   mov ah, Screen_kBlue ; Color (head)
-  mov [Player_left][TAG_kColor], ax ; color = (White:White)
+  Player_leftSet TAG_kColor, ax ; color = (White:White)
 
   mov ax, Input_kRight
-  mov [Player_left][TAG_kDirection], ax ; direction = None
+  Player_leftSet TAG_kDirection, ax ; direction = None
 
   mov ax, Screen_kWidth
   inc ax
-  mov [Player_left][TAG_kPosition], ax
+  Player_leftSet TAG_kPosition, ax
 
-  mov ax, offset [Player_left][TAG_kTail]
-  mov [Player_left][TAG_kIndex], ax ; index = Tail[0]
+  lea ax, [Game_data + TAG_kTails][0*02h]
+  Player_leftSet TAG_kIndex, ax ; index = Tail[0]
 
   ; Allocate right player
   mov al, Screen_kYellow ; Color (tail)
   mov ah, Screen_kRed ; Color (head)
-  mov [Player_right][TAG_kColor], ax ; color = (White:White)
+  Player_rightSet TAG_kColor, ax ; color = (White:White)
 
   mov ax, Input_kLeft
-  mov [Player_right][TAG_kDirection], ax ; direction = None
+  Player_rightSet TAG_kDirection, ax ; direction = None
 
   mov ax, Screen_kWidth * 2
   sub ax, 2
-  mov [Player_right][TAG_kPosition], ax
+  Player_rightSet TAG_kPosition, ax
 
-  mov ax, offset [Player_right][TAG_kTail]
-  add ax, 2
-  mov [Player_right][TAG_kIndex], ax ; index = Tail[0]
+  lea ax, [Game_data + TAG_kTails][1*02h]
+  Player_rightSet TAG_kIndex, ax ; index = Tail[1]
 
   pop ds
   mov sp, bp
@@ -131,24 +163,24 @@ Player_init proc near ; IO () {{{1
   xor bh, bh
 
   ; Initialize Jump Table
-  mov ax, offset Player_moveNone
+  lea ax, Player_moveNone
   mov [Player_directions][Input_kNone], ax
 
   mov al, Input_kUp
   and al, Input_kVerticalMask
   mov bl, al
-  mov ax, offset Player_moveUp
+  lea ax, Player_moveUp
   mov [Player_directions][bx], ax
 
   mov al, Input_kDown
   and al, Input_kVerticalMask
   mov bl, al
-  mov ax, offset Player_moveDown
+  lea ax, Player_moveDown
   mov [Player_directions][bx], ax
 
-  mov ax, offset Player_moveLeft
+  lea ax, Player_moveLeft
   mov [Player_directions][Input_kLeft], ax
-  mov ax, offset Player_moveRight
+  lea ax, Player_moveRight
   mov [Player_directions][Input_kRight], ax
 
   pop ds
@@ -177,28 +209,36 @@ Player_input proc near ; IO () {{{1
   cmp al, Input_kNone
   je  @right
 
-  mov bx, [Player_left][TAG_kDirection]
+  push ax
+  Player_leftGet TAG_kDirection
+  mov bx, ax
+  pop ax
+
   and bl, Input_kVerticalFlag
   mov bh, al
   and bh, Input_kVerticalFlag
   cmp bl, bh
   je  @right ; did not change horizontal/vertical
 
-  mov [Player_left][TAG_kDirection], ax
+  Player_leftSet TAG_kDirection, ax
 
 @right:
   call Input_wsdaKeys
   cmp al, Input_kNone
   je  @done
 
-  mov bx, [Player_right][TAG_kDirection]
+  push ax
+  Player_rightGet TAG_kDirection
+  mov bx, ax
+  pop ax
+
   and bl, Input_kVerticalFlag
   mov bh, al
   and bh, Input_kVerticalFlag
   cmp bl, bh
   je  @done ; did not change horizontal/vertical
 
-  mov [Player_right][TAG_kDirection], ax
+  Player_rightSet TAG_kDirection, ax
 
 @done:
 
@@ -246,17 +286,14 @@ Player_renderTail proc near ; (player) -> IO () {{{1
   mov ax, [bx][TAG_kColor]
   mov dx, ax ; dl <- Player (_:color)
 
-  ; TODO: lea ax, [bx][TAG_kIndex]
-  mov ax, bx
-  add ax, TAG_kIndex
-  mov bx, ax
+  push bx
+  lea bx, [bx][TAG_kIndex]
   mov ax, [bx]
   mov si, ax ; si <- Player[index]
+  pop bx
 
-  mov ax, [bp + 4][0] ; player
-  mov bx, ax
-
-  sub si, 4
+  lea bx, [Game_data + TAG_kTails]
+  sub si, 2*02h
 
 @@:
   push dx
@@ -266,12 +303,10 @@ Player_renderTail proc near ; (player) -> IO () {{{1
 
   ; Increment word offset (= 2 bytes)
   ; Skip over other player
-  sub si, 4
+  sub si, 2*02h
 
-  mov ax, bx
-  add ax, TAG_kTail
-  cmp si, ax
-  ; TODO: fails with jg <= signed intergers
+  cmp si, bx
+  ; TODO: explain jea (unsigned) vs jg (signed)
   jae @B ; unless(bx == END)
 
   pop ds
@@ -375,7 +410,7 @@ Player_update proc near ; (player) {{{1
   push bx
   call Player_setTail ; (player)
 
-  add word ptr [bx][TAG_kIndex], 4
+  add word ptr [bx][TAG_kIndex], 2*02h
 
   mov ax, [bx][TAG_kDirection]
   mov bx, ax
@@ -517,6 +552,102 @@ Player_setTail proc near ; (player) -> IO () {{{1
   pop bp
   ret 2 ; (player)
 Player_setTail endp
+
+Level_init proc near ; {{{1
+  push bp
+  mov  bp, sp
+  push bx
+  push cx
+  push dx
+  push ds
+
+  ; Data Segment
+  mov ax, @fardata
+  mov ds, ax
+  lea bx, [Game_data + TAG_kLevel]
+
+  mov cx, Screen_kWidth
+@@:
+  mov [bx], cx
+
+  add bx, 1*02h
+  dec cx
+  cmp cx, 0
+  jne @B
+
+  mov cx, Screen_kWidth*Screen_kHeight
+@@:
+  mov [bx], cx
+
+  add bx, 1*02h
+  dec cx
+  cmp cx, Screen_kWidth*Screen_kHeight - Screen_kWidth
+  jne @B
+
+  mov cx, Screen_kWidth*Screen_kHeight - Screen_kWidth
+@@:
+  mov [bx], cx
+
+  add bx, 1*02h
+  sub cx, Screen_kWidth
+  cmp cx, 0
+  jne @B
+
+  mov cx, Screen_kWidth*Screen_kHeight - 1
+@@:
+  mov [bx], cx
+
+  add bx, 1*02h
+  sub cx, Screen_kWidth
+  cmp cx, Screen_kWidth - 1
+  jne @B
+
+  pop ds
+  pop dx
+  pop cx
+  pop bx
+  mov sp, bp
+  pop bp
+  ret
+Level_init endp
+
+Level_render proc near ; {{{1
+  push bp
+  mov  bp, sp
+  push bx
+  push dx
+  push si
+  push ds
+
+  ; Data Segment
+  mov ax, @fardata
+  mov ds, ax
+
+  mov dx, Screen_kWhite
+  lea si, [Game_data + TAG_kTails] ; end
+  lea bx, [Game_data + TAG_kLevel] ; begin
+
+  sub si, 1*02h ; skip Tails[0]
+
+@@:
+  push dx
+  mov  ax, [si]
+  push ax
+  call Screen_setPixel ; (color, position)
+
+  sub si, 1*02h
+
+  cmp si, bx
+  jae @B ; unless(bx == END)
+
+  pop ds
+  pop si
+  pop dx
+  pop bx
+  mov sp, bp
+  pop bp
+  ret
+Level_render endp
 
 ; }}}1
 
